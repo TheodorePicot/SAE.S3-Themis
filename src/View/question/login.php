894f773d3d52eee8459b315e2de90caab7268379
michelu <ugo.michel01@etu.umontpellier.fr>
2022-10-19 17:09:49 +0200

    Formulaire login et sign up

---
+++ src/View/question/login.php
@@ -0,0 +1,21 @@
+<form method="get" action="frontController.php">
+    <fieldset>
+        <legend>Se connecté</legend>
+
+        <p>
+            <label for="email">Email</label> :
+            <input type="text" placeholder="dupontpaul1610@gmail.com" name="email" id="email" required/>
+        </p>
+
+        <p>
+            <label for="mdp">Mot de passe</label> :
+            <input type="password" name="mdp" id="mdp" minlength="8" required/>
+        </p>
+
+        <p>
+            <input type="submit" value="Envoyer" />
+            <a href="signUp..php"> S'incrire </a>
+        </p>
+
+    </fieldset>
+</form>
