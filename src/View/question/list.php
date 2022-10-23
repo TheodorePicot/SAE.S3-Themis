e0a260d828271b9507b328475703bd1c5bebb625
TheodorePicot <95174585+TheodorePicot@users.noreply.github.com>
2022-10-23 20:31:11 +0200

    Questionclickable

--- src/View/question/list.php
+++ src/View/question/list.php
@@ -1,17 +1,14 @@
 <?php
 
 foreach ($questions as $question) {
-    $questionInHTML = htmlspecialchars($question->getIdQuestion());
+    $questionInHTML = htmlspecialchars($question->getTitreQuestion());
     $questionInURL = rawurlencode($question->getIdQuestion());
     $hrefRead = "frontController.php?action=read&idQuestion=" . $questionInURL;
 
-
-
-    echo '
-    <div id="test" class="mx-2 my-5 container-fluid" style="border:2px solid #cecece;">
-   
-    <p> Question d\'ID ' . " <a href='$hrefRead'>" . $questionInHTML . '</a>' . '</p>
-    
-    </div>';
+    echo "<a href='$hrefRead'>";
+    echo "<div id='test' class='mx-2 my-5 container-fluid' style='border:2px solid #cecece;'>
+            <p> Question d\'ID . $questionInHTML </p>
+          </div>
+          </a>";
 
 }
