d6ecc9feaba421fa5aa02ec66d944e7db6ad4639
Jihan <jihanhajem@gmail.com>
2022-10-24 22:31:18 +0200

    font css

--- src/View/view.php
+++ src/View/view.php
@@ -8,22 +8,26 @@
 
     <link rel="stylesheet" href="css/style.css">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
+
 </head>
 <body>
 <header>
     <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light bg-transparent fs-5">
             <!-- Navbar content -->
-            <a class="navbar-brand">Themis</a>
-            <ul class="navbar-nav">
+            <a class="navbar-brand"><h1>Themis</h1></a>
+            <ul class="navbar-nav ms-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="frontController.php?action=readAll">Questions</a>
+                    <a class="nav-link" href="frontController.php?action=readAll"><h4>Questions</h4></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="frontController.php?action=create">Creer Question</a>
+                    <a class="nav-link" href="frontController.php?action=create"><h4>Creer Question</h4></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Test</a>
+                    <a class="nav-link" href="#"><h4>Test</h4></a>
                 </li>
             </ul>
         </nav>
