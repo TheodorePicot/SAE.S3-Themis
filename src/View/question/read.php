67aa7aba2ef977a2821898e1ec1b3ca7aa6beedf
JihanH <jihanhajem@gmail.com>
2022-11-05 16:08:01 +0100

    update bouton delete update

--- src/View/question/read.php
+++ src/View/question/read.php
@@ -1,44 +1,53 @@
 <?php
-    $questionInURL = rawurlencode($question->getIdQuestion());
-    $hrefDelete = "frontController.php?action=delete&idQuestion=" . $questionInURL;
-    $hrefUpdate = "frontController.php?action=update&idQuestion=" . $questionInURL;
-    $hrefCreateSection = "frontController.php?action=created&controller=section&idQuestion=$questionInURL";
-    $hrefReadAll = "frontController.php?action=readAll";
-    $lienRetourQuestion = "<a href=".$hrefReadAll.">Questions : </a>";
+$questionInURL = rawurlencode($question->getIdQuestion());
+$hrefDelete = "frontController.php?action=delete&idQuestion=" . $questionInURL;
+$hrefUpdate = "frontController.php?action=update&idQuestion=" . $questionInURL;
+$hrefCreateSection = "frontController.php?action=created&controller=section&idQuestion=$questionInURL";
+$hrefReadAll = "frontController.php?action=readAll";
+$lienRetourQuestion = "<a href=" . $hrefReadAll . ">Questions : </a>";
 ?>
 
 <div class='container my-5'>
     <p>
     <ul style='list-style: none'>
         <li>
-           <h1> <?= htmlspecialchars($question->getTitreQuestion()) ?></h1>
+            <h1> <?= htmlspecialchars($question->getTitreQuestion()) ?></h1>
         </li>
         <li>
-            Description de la question : <?=htmlspecialchars($question->getDescriptionQuestion())?>
-        </li>
-         <li>
-            Date de début de proposition : <?=htmlspecialchars($question->getDateDebutProposition())?>
-        </li>
-         <li>
-            Date de fin de rédaction de proposition : <?=htmlspecialchars($question->getDateFinProposition())?>
-        </li>
-         <li>
-            Date de début de vote : <?=htmlspecialchars($question->getDateDebutVote())?>
+            Description de la question : <?= htmlspecialchars($question->getDescriptionQuestion()) ?>
         </li>
         <li>
-            Date de fin de vote : <?=htmlspecialchars($question->getDateFinVote())?>
+            Date de début de proposition : <?= htmlspecialchars($question->getDateDebutProposition()) ?>
+        </li>
+        <li>
+            Date de fin de rédaction de proposition : <?= htmlspecialchars($question->getDateFinProposition()) ?>
+        </li>
+        <li>
+            Date de début de vote : <?= htmlspecialchars($question->getDateDebutVote()) ?>
+        </li>
+        <li>
+            Date de fin de vote : <?= htmlspecialchars($question->getDateFinVote()) ?>
         </li>
         </p>
 
 
-    <?php require_once __DIR__ . "/../section/listByQuestionForRead.php" ?>
+        <?php require_once __DIR__ . "/../section/listByQuestionForRead.php" ?>
 
-    <div id='containerDeleteUpdate'>
-        <ul style='list-style: none'>
-            <li> <a href='<?=$hrefDelete?>'>delete</a> </li>
-            <li> <a href='<?=$hrefUpdate?>'>update</a> </li>
-        </ul>
-    </div>
+
+        <div id="containerUpdateDelete">
+            <ul style='list-style: none'>
+                <a href='<?= $hrefDelete ?>'>
+                <div class="my-1" style='border:1px solid; border-radius: 10px'>
+                    <li>delete</li>
+                </div>
+                </a>
+                <a href='<?= $hrefUpdate?>'>
+                <div class="my-1" style='border:1px solid; border-radius: 10px' href='<?= $hrefUpdate ?>'>
+                    <li>update</li>
+                </div>
+                </a>
+            </ul>
+        </div>
     </ul>
 </div>
 
--- src/View/section/listByQuestionForRead.php
+++ src/View/section/listByQuestionForRead.php
@@ -2,8 +2,8 @@
 $count = 1;
 foreach ($sections as $section) : ?>
 
-    <h3> Section <?=$count?> </h3>
-     <p><?=$section->getTitreSection()?>
+<!--    <h3> Section --><?//=$count?><!-- </h3>-->
+   <h3>  <p><?=$count.". ".$section->getTitreSection()?> </h3>
      <?=$section->getDescriptionSection()?></p>
 <?php
 $count++;
