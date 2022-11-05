78a819683f904b64e6b389a845d4b69fa4749ebb
JihanH <jihanhajem@gmail.com>
2022-11-05 20:52:41 +0100

    update css

--- src/View/question/list.php
+++ src/View/question/list.php
@@ -5,10 +5,12 @@
     $questionInURL = rawurlencode($question->getIdQuestion());
     $hrefRead = "frontController.php?action=read&idQuestion=" . $questionInURL;
 ?>
+
     <a id='containerQuestion' class = container-fluid' href='<?=$hrefRead?>'>
-       <div id ="question" class="box my-2" style='border:1px solid; border-radius: 10px'>
-                <p> <?=$titreQuestionHTML?> </p>
+       <div id ="question" class="box my-2" style=' border-radius: 10px'>
+                <h5><p> <?=$titreQuestionHTML?> </p></h5>
        </div>
     </a>
-<?php endforeach; ?>
+
+<?php endforeach;?>
 </div>
