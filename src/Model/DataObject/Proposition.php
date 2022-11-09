508b6d3bf5c33f53bc52c89c98c9a9406ab38a92
michell <leo.michel02@etu.umontpellier.fr>
2022-11-09 21:49:35 +0100

    class Proposition.php

--- src/Model/DataObject/Proposition.php
+++ src/Model/DataObject/Proposition.php
@@ -1,6 +1,52 @@
 <?php
 
+namespace Themis\Model\DataObject;
 
 class Proposition extends AbstractDataObject {
 
+    private int $idProposition;
+    private int $idQuestion;
+
+    /**
+     * @param int $idProposition
+     * @param int $idQuestion
+     */
+    public function __construct(int $idProposition, int $idQuestion)
+    {
+        $this->idProposition = $idProposition;
+        $this->idQuestion = $idQuestion;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdProposition(): int
+    {
+        return $this->idProposition;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdQuestion(): int
+    {
+        return $this->idQuestion;
+    }
+
+    public function tableFormatWithoutPrimaryKey(): array
+    {
+        // TODO: Implement tableFormatWithoutPrimaryKey() method.
+        return [
+            "idProposition" => $this->idProposition,
+            "idQuestion" => $this->idQuestion
+        ];
+    }
+
+    public function tableFormatWithPrimaryKey(): array
+    {
+        // TODO: Implement tableFormatWithPrimaryKey() method.
+        return [
+            "idQuestion" => $this->idQuestion
+        ];
+    }
 }
