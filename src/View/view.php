30021cbb36f16022c285c14820cf05f8d3cf131f
JihanH <jihanhajem@gmail.com>
2022-11-10 12:31:02 +0100

    fixNavBar

--- src/View/view.php
+++ src/View/view.php
@@ -83,15 +83,10 @@
                         <a class="nav-link" href="frontController.php?action=create">Créer une Question</a>
                     </li>
                 </ul>
-                <form class="d-flex">
-                    <a class="btn btn-outline-success" href="frontController.php?controller=utilisateur&action=create">Sign Up</a>
-<!--                    <button class="btn btn-outline-success" href="frontController.php?controller=utilisateur&action=login">Login</button>-->
-                </form>
-
-<!--                                <form class="d-flex">-->
-<!--                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">-->
-<!--                                    <button class="btn btn-outline-success" type="submit">Search</button>-->
-<!--                                </form>-->
+                <!--                <form class="d-flex">-->
+                <!--                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">-->
+                <!--                    <button class="btn btn-outline-success" type="submit">Search</button>-->
+                <!--                </form>-->
             </div>
         </div>
     </nav>
