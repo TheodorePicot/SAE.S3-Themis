a2f680bfcdad5383b0d91d974ac1b2fce53508f7
michell <leo.michel02@etu.umontpellier.fr>
2022-11-10 13:08:03 +0100

    avancement

--- src/Controller/ControllerProposition.php
+++ src/Controller/ControllerProposition.php
@@ -3,6 +3,7 @@
 namespace Themis\Controller;
 
 use Themis\Model\Repository\PropositionRepository;
+use Themis\Model\Repository\SectionPropositionRepository;
 
 class ControllerProposition extends AbstactController {
     protected function getCreationMessage(): string
@@ -15,8 +16,7 @@ class ControllerProposition extends AbstactController {
         return "proposition";
     }
 
-    //en cours
-    /*
+
     public function created(): void
     {
         $proposition = (new PropositionRepository())->build($_GET);
@@ -24,8 +24,12 @@ class ControllerProposition extends AbstactController {
         if ((new PropositionRepository)->create($proposition)) {
             $idProposition = DatabaseConnection::getPdo()->lastInsertId(); // Cette fonction nous permet d'obtenir l'id du dernier objet inséré dans une table.
 
-            $sections = (new SectionRepository)->selectAllByQuestion($idQuestion); //retourne un tableau de toutes les sections d'une question
-            $question = (new QuestionRepository)->select($idQuestion);
+            $sections = (new SectionRepository)->selectAllByQuestion($idProposition); //retourne un tableau de toutes les sections d'une question
+            $question = (new QuestionRepository)->select($idProposition);
+
+            foreach ($sections as $section){
+                (new SectionPropositionRepository())->build(array('texteProposition'=>'', 'idSection'=>$section->getIdSection()));
+            }
 
             $this->showView("view.php", [
                 "sections" => $sections,
@@ -37,5 +41,4 @@ class ControllerProposition extends AbstactController {
             $this->showError("Erreur de création de la question");
         }
     }
-    */
 }
