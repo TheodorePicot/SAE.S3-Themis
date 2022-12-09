9f58e158643664c4e5b026e0f0a18919d70e3b35
Jihan <jihanhajem@gmail.com>
2022-12-10 00:05:39 +0100

    still updating the user's view...

--- src/View/utilisateur/read.php
+++ src/View/utilisateur/read.php
@@ -10,45 +10,54 @@ $hrefUpdate = "frontController.php?action=update&controller=utilisateur&login=" 
 
 <div class="container-fluid">
     <div class="offset-4 row my-5">
-        <div class="col col-lg-12">
-            <h1>
+        <div class="col col-lg-12 my-5">
+            <h3>
                 <img id="accountImgPage" alt="compte" src="assets/img/account.png">
-                <?= htmlspecialchars($utilisateur->getLogin()) ?> </h1>
+                <?= htmlspecialchars($utilisateur->getLogin()) ?>
+                <small class="text-muted">
+                    <?php if ($utilisateur->isAdmin()) : ?>
+                        Administrateur
+                    <?php elseif ($utilisateur->isOrganisateur()): ?>
+                        Droits
+                        Organisateur
+                        <!--                --><?php //else : ?>
+                        <!--                    Droits-->
+                        <!--                    Utilisateur-->
+                    <?php endif ?>
+                </small>
+            </h3>
+
+
         </div>
-        <div class="col-lg-3 my-lg-5 ">
+        <div class="col-lg-3 ">
             <h3>Prenom <br></h3>
-            <div class="" style="background:;"
-            ">
+
             <?= htmlspecialchars($utilisateur->getPrenom()) ?><br>
+            <div class="my-lg-3">
+                <h3>Adresse email <br></h3>
+
+                <?= htmlspecialchars($utilisateur->getAdresseMail()) ?><br>
+            </div>
         </div>
-        <h3>Adresse email <br></h3>
-        <?= htmlspecialchars($utilisateur->getAdresseMail()) ?><br>
-        <?php if ($utilisateur->isAdmin()) : ?>
-            <h3> Droits <br></h3>
-            Administrateur
-        <?php elseif ($utilisateur->isOrganisateur()): ?>
-            Droits <br>
-            Organisateur
-        <?php else : ?>
-            Droits <br>
-            Utilisateur
-        <?php endif ?>
-    </div>
-    <div class="col-lg-3 my-lg-5 ">
-        <h3>Nom <br></h3>
-        <?= htmlspecialchars($utilisateur->getNom()) ?><br>
-        <h3>Date de Naissance <br></h3>
-        <?= htmlspecialchars($utilisateur->getDateNaissance()) ?><br>
-    </div>
+        <div class="col-lg-3">
+            <h3>Nom <br></h3>
+            <?= htmlspecialchars($utilisateur->getNom()) ?><br>
+            <div class="my-lg-3">
+                <h3>Date de Naissance <br></h3>
+                <?= htmlspecialchars($utilisateur->getDateNaissance()) ?><br>
+            </div>
+        </div>
 
-    <div class="col-lg-12 my-5">
-        <?php if (ConnexionUtilisateur::isUser($utilisateurInURL) || ConnexionUtilisateur::isAdministrator()) : ?>
-            <a class="btn btn-dark text-nowrap mx-1" href='<?= $hrefDelete ?>'> Supprimer</a>
-            <a class="btn btn-dark text-nowrap" href='<?= $hrefUpdate ?>'> Mettre à jour</a>
-        <?php endif ?>
+
+        <div class="offset-1 col-lg-12 my-5">
+            <?php if (ConnexionUtilisateur::isUser($utilisateurInURL) || ConnexionUtilisateur::isAdministrator()) : ?>
+                <a class="btn btn-dark text-nowrap mx-1" href='<?= $hrefDelete ?>'> Supprimer</a>
+                <a class="btn btn-dark text-nowrap" href='<?= $hrefUpdate ?>'> Mettre à jour</a>
+            <?php endif ?>
+        </div>
     </div>
 </div>
 
 
-</div>
+
 
