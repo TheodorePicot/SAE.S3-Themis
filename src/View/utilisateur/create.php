01f83688549887b4c7a9b9deec0135b26f28d487
Jihan <jihanhajem@gmail.com>
2022-12-10 12:40:21 +0100

    update login+create

--- src/View/utilisateur/create.php
+++ src/View/utilisateur/create.php
@@ -1,100 +1,104 @@
+
+
 <div class="container-fluid">
-    <div class="row my-5 mx-5">
-        <div class="shadowBox col-md-12 offset-lg-3 col-lg-6 my-4">
-            <div class="card-body border-0 rounded-4" style="background: #C7B198;">
-                <form method="get" action="frontController.php">
-                    <fieldset>
-                        <div class="d-flex align-content-center justify-content-center">
-                            <h1> Bienvenue sur Themis</h1>
+    <div class="row my-5 ">
+        <div class=" col-md-12 offset-lg-4 col-lg-4 my-4">
+            <form method="get" action="frontController.php">
+                <fieldset>
+                    <div class="d-flex align-content-center justify-content-center">
+                        <h1 class="display-3"> Bienvenue sur Themis</h1>
+                    </div>
+                    <div class="my-4">
+                        <input type="hidden" name="controller" value="utilisateur">
+                        <input type="hidden" name="action" value="created">
+
+                        <div class="mx-3">
+                            <h5><label for="login">Login</label></h5>
+                            <p>
+                                <input class="form-control" type="text" placeholder="Paul16" name="login"
+                                       id="login"
+                                       required/>
+                            </p>
                         </div>
-                        <div class="my-4">
-                            <input type="hidden" name="controller" value="utilisateur">
-                            <input type="hidden" name="action" value="created">
-
-                            <div class="mx-3">
-                                <h5><label for="login">Login</label></h5>
-                                <p>
-                                    <input class="form-control" type="text" placeholder="Paul16" name="login"
-                                           id="login"
-                                           required/>
-                                </p>
-                            </div>
 
 
-                            <div class="mx-3">
-                                <h5><label for="adresseMail">Email&#42;</label></h5>
-                                <p>
-                                    <input class="form-control" type="text" placeholder="dupontpaul1610@gmail.com"
-                                           name="adresseMail"
-                                           id="adresseMail"
-                                           required/>
-                                </p>
-                            </div>
+                        <div class="mx-3">
+                            <h5><label for="adresseMail">Email&#42;</label></h5>
+                            <p>
+                                <input class="form-control" type="text" placeholder="dupontpaul1610@gmail.com"
+                                       name="adresseMail"
+                                       id="adresseMail"
+                                       required/>
+                            </p>
+                        </div>
 
-                            <div class="mx-3">
-                                <h5><label for="nom">Nom</label></h5>
-                                <p>
-                                    <input class="form-control" type="text" placeholder="Dupont" name="nom" id="nom"
-                                           required/>
-                                </p>
-                            </div>
+                        <div class="mx-3">
+                            <h5><label for="nom">Nom</label></h5>
+                            <p>
+                                <input class="form-control" type="text" placeholder="Dupont" name="nom" id="nom"
+                                       required/>
+                            </p>
+                        </div>
 
-                            <div class="mx-3">
-                                <h5><label for="prenom">Prénom</label></h5>
-                                <p>
-                                    <input class="form-control" type="text" placeholder="Paul" name="prenom" id="prenom"
-                                           required/>
-                                </p>
-                            </div>
-                            <div class="mx-3">
-                                <h5><label for="dateNaissance">Date de naissance</label></h5>
-                                <p>
-                                    <input class="form-control" type="date" name="dateNaissance" id="dateNaissance"
-                                           required/>
-                                </p>
-                            </div>
+                        <div class="mx-3">
+                            <h5><label for="prenom">Prénom</label></h5>
+                            <p>
+                                <input class="form-control" type="text" placeholder="Paul" name="prenom" id="prenom"
+                                       required/>
+                            </p>
+                        </div>
+                        <div class="mx-3">
+                            <h5><label for="dateNaissance">Date de naissance</label></h5>
+                            <p>
+                                <input class="form-control" type="date" name="dateNaissance" id="dateNaissance"
+                                       required/>
+                            </p>
+                        </div>
 
-                            <div class="mx-3 ">
-                                <h5><label for="mdp">Mot de passe&#42;</label></h5>
-                                <p>
-                                    <input class="form-control" type="password" name="mdp" id="mdp" minlength="8"
-                                           required/>
-                                </p>
-                            </div>
+                        <div class="mx-3 ">
+                            <h5><label for="mdp">Mot de passe&#42;</label></h5>
+                            <p>
+                                <input class="form-control" type="password" name="mdp" id="mdp" minlength="8"
+                                       required/>
+                            </p>
+                        </div>
 
-                            <div class="mx-3 ">
-                                <h5><label for="mdpConfirmation">Confirmation mot de passe&#42;</label></h5>
-                                <p>
-                                    <input class="form-control" type="password" name="mdpConfirmation" id="mdpConfirmation" minlength="8"
-                                           required/>
-                                </p>
-                            </div>
+                        <div class="mx-3 ">
+                            <h5><label for="mdpConfirmation">Confirmation mot de passe&#42;</label></h5>
+                            <p>
+                                <input class="form-control" type="password" name="mdpConfirmation" id="mdpConfirmation"
+                                       minlength="8"
+                                       required/>
+                            </p>
+                        </div>
 
-                            <?php use Themis\Lib\ConnexionUtilisateur;
+                        <?php use Themis\Lib\ConnexionUtilisateur;
 
-                            if (ConnexionUtilisateur::isAdministrator()) :?>
+                        if (ConnexionUtilisateur::isAdministrator()) :?>
                             <div class="mx-3 ">
                                 <h5><label class="InputAddOn-item" for="estAdmin_id">Administrateur</label></h5>
                                 <p>
-                                    <input class="InputAddOn-field" type="checkbox" placeholder="" name="estAdmin" id="estAdmin_id">
+                                    <input class="InputAddOn-field" type="checkbox" placeholder="" name="estAdmin"
+                                           id="estAdmin_id">
                                 </p>
                             </div>
                             <div class="mx-3 ">
                                 <h5><label class="InputAddOn-item" for="estOrganisateur">Organisateur</label></h5>
                                 <p>
-                                    <input class="InputAddOn-field" type="checkbox" placeholder="" name="estOrganisateur" id="estOrganisateur">
+                                    <input class="InputAddOn-field" type="checkbox" placeholder=""
+                                           name="estOrganisateur" id="estOrganisateur">
                                 </p>
                             </div>
-                            <?php endif ?>
-                            <div class="d-flex align-content-center justify-content-center my-5">
+                        <?php endif ?>
+                        <div class="d-flex align-content-center justify-content-center my-5">
 
-                                <input class="btn btn-dark" type="submit" value="S'enregistrer"/>
-                            </div>
-
+                            <input class="btn btn-dark" type="submit" value="S'enregistrer"/>
                         </div>
-                    </fieldset>
-                </form>
-            </div>
+
+                    </div>
+                </fieldset>
+            </form>
+
             <div class="col-md-1 col-lg-4">
 
             </div>
--- src/View/utilisateur/login.php
+++ src/View/utilisateur/login.php
@@ -1,30 +1,30 @@
+<?php
+$hrefCreateAccount = "frontController.php?controller=utilisateur&action=create";
+?>
 <div class="container" xmlns="http://www.w3.org/1999/html">
     <div class="row my-5">
-
-        <div class="col-lg-4">
-        </div>
-
-        <div class="shadowBox col-lg-4 container my-4 card-body border-0 rounded-4" style="background: #C7B198;">
+        <div class=" col-lg-4 container my-5">
             <form method="get" action="frontController.php">
                 <fieldset>
-                    <div class="d-flex align-content-center justify-content-center my-1">
-                        <h2>
-                            Se connecter
-                        </h2>
+                    <div class="d-flex align-content-center justify-content-center my-5">
+                        <h1 class="display-2">
+                            Themis
+                        </h1>
                     </div>
                     <div class="my-4">
                         <div class="mx-3">
-                            <h5><label for="login">Login</label></h5>
+                            <!--                            <h5><label for="login">Pseudo</label></h5>-->
                             <p>
-                                <input class="form-control" type="text" placeholder="dupontpaul1610" name="login"
+                                <input class="form-control form-control-lg" type="text" placeholder="pseudo"
+                                       name="login"
                                        id="login"
                                        required/>
                             </p>
                         </div>
                         <div class="mx-3">
-                            <h5><label for="mdp">Mot de passe&#42;</label></h5>
-
-                            <input class="form-control" type="password" name="mdp" id="mdp" minlength="8"
+                            <!--                            <h5><label for="mdp">Mot de passe&#42;</label></h5>-->
+                            <input class="form-control form-control-lg" type="password" placeholder="mot de passe"
+                                   name="mdp" id="mdp" minlength="8"
                                    required/>
 
                         </div>
@@ -32,13 +32,17 @@
                         <input type='hidden' name='action' value='connect'>
                         <input type='hidden' name='controller' value='utilisateur'>
                         <div class="d-flex align-content-center justify-content-center my-5">
-                            <button class="btn btn-dark" type="submit">  Se connecter </button>
+                            <button class="btnLogin btn btn-lg btn-dark bg-transparent" type="submit"> Se connecter
+                            </button>
                         </div>
+                        <p class="d-flex align-content-center justify-content-center">
+                            Vous n'avez pas de compte ?&nbsp;
+                            <a href="<?= $hrefCreateAccount ?>"> inscrivez-vous!</a>
+                        </p>
                     </div>
 
                 </fieldset>
             </form>
         </div>
-        <div class="col-lg-4">
-        </div>
+
     </div>
