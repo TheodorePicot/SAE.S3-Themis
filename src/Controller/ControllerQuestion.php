c52f1c39dc2a2b10ddb3bb9654797f1bc8bf13e2
Jihan <jihanhajem@gmail.com>
2023-01-14 21:08:39 +0100

    méthode getlike supprimé

--- src/Controller/ControllerQuestion.php
+++ src/Controller/ControllerQuestion.php
@@ -158,7 +158,7 @@ class ControllerQuestion extends AbstractController
         (new QuestionRepository)->update($question);
 
         foreach ((new SectionRepository)->selectAllByQuestion($question->getIdQuestion()) as $section) {
-            $updatedSection = new Section($section->getIdSection(), $section->getIdQuestion(), $_REQUEST["titreSection{$section->getIdSection()}"], $_REQUEST["descriptionSection{$section->getIdSection()}"], $section->getLike());
+            $updatedSection = new Section($section->getIdSection(), $section->getIdQuestion(), $_REQUEST["titreSection{$section->getIdSection()}"], $_REQUEST["descriptionSection{$section->getIdSection()}"]);
             (new SectionRepository)->update($updatedSection);
         }
 
